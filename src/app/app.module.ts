c3930f59efba77a2a60487cacc2eb1230c6d281d
Gatien Tafforeau <gta@hq.ivalua.com>
2018-03-14 16:36:37 +0100

    Ajout route

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -16,6 +16,12 @@ import { ManageProductsComponent } from './manage-products/manage-products.compo
 import { ManageOrdersComponent } from './manage-orders/manage-orders.component';
 import { OrderComponent } from './order/order.component';
 import { ProductsQuantityComponent } from './products-quantity/products-quantity.component';
+import { RouterModule, Routes } from '@angular/router';
+
+const appRoutes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: '/menu', component: MenuComponent}
+]
 
 @NgModule({
   declarations: [
@@ -32,6 +38,10 @@ import { ProductsQuantityComponent } from './products-quantity/products-quantity
     ProductsQuantityComponent
   ],
   imports: [
+    RouterModule.forRoot(
+          appRoutes,
+          { enableTracing: false } // <-- debugging purposes only
+        ),
     BrowserModule,
     ServiceWorkerModule.register('/ngsw-worker.js', { enabled: environment.production })
   ],
