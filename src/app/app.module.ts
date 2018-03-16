16574f542a844f61cf16d22f3973b6d3362fb5ca
Antislash <gtaffor@gmail.com>
2018-03-16 21:18:14 +0100

    Correction route

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -20,7 +20,7 @@ import { RouterModule, Routes } from '@angular/router';
 
 const appRoutes: Routes = [
   { path: '', component: HomeComponent },
-  { path: '/menu', component: MenuComponent}
+  { path: 'menu', component: MenuComponent}
 ]
 
 @NgModule({
