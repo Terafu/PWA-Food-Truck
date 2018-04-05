4c7d3d9028ed8a879b91ade366b43760b93ce996
Terafu <maxime-dufour@outlook.fr>
2018-04-05 14:50:10 +0200

    Add quick View

--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Product } from '../models/product';
 import {ProductService} from '../services/product.service';
+import {ModalDismissReasons, NgbModal} from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-product',
@@ -8,13 +9,31 @@ import {ProductService} from '../services/product.service';
   styleUrls: ['./product.component.css']
 })
 export class ProductComponent implements OnInit {
+  closeResult: string;
 
   @Input()
-  	product: Product;
+  product: Product;
 
-  constructor(private articleService: ProductService) { }
+  constructor(private articleService: ProductService, private modalService: NgbModal) { }
 
   ngOnInit() {
   }
 
+  open(content) {
+    this.modalService.open(content).result.then((result) => {
+      this.closeResult = `Closed with: ${result}`;
+    }, (reason) => {
+      this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+    });
+  }
+
+  private getDismissReason(reason: any): string {
+    if (reason === ModalDismissReasons.ESC) {
+      return 'by pressing ESC';
+    } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
+      return 'by clicking on a backdrop';
+    } else {
+      return  `with: ${reason}`;
+    }
+  }
 }
